7032c0e5e931806f19faba281e4de4fe2ad903ec
AitorCalderonLo <aitor.calderonlo@elorrieta-errekamari.com>
2022-07-14 11:09:01 +0200

    error handling for login operations

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {catchError} from "rxjs/operators";
 import {throwError} from "rxjs";
 
@@ -20,13 +20,38 @@ export class AuthService {
   constructor(private http: HttpClient) {
   }
 
+  private handleError(errorRes: HttpErrorResponse){
+    let errorMessage = 'An uknown error ocurred.'
+    if (!errorRes.error || !errorRes.error.error){
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'Email exists';
+        break;
+      case 'OPERATION_NOT_ALLOWED':
+        errorMessage = 'Operation not allowed, please contact with the administrator';
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMessage = 'Too many sign up attempts, try again later';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'Account not found';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'The password is invalid or the user does not have a password';
+        break;
+    }
+    return throwError(errorMessage);
+  }
+
   login(email: string, password: string,){
     return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBgZUcL-r-8EQ1QTuSCRplRCREvYzAorZY',
       {
         email: email,
         password: password,
         returnSecureToken: true
-      })
+      }).pipe(catchError(this.handleError));
   }
 
   signUp(email: string, password: string,) {
@@ -35,24 +60,6 @@ export class AuthService {
         email: email,
         password: password,
         returnSecureToken: true
-      }).pipe(catchError(errorRes => {
-        let errorMessage = 'An uknown error ocurred.'
-      if (!errorRes.error || !errorRes.error.error){
-        return throwError(errorMessage);
-      }
-      switch (errorRes.error.error.message) {
-        case 'EMAIL_EXISTS':
-          errorMessage = 'Email exists';
-          break;
-        case 'OPERATION_NOT_ALLOWED':
-          errorMessage = 'Operation not allowed, please contact with the administrator';
-          break;
-        case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-          errorMessage = 'Too many sign up attempts, try again later';
-          break;
-      }
-      return throwError(errorMessage);
-
-    }))
+      }).pipe(catchError(this.handleError));
   }
 }
